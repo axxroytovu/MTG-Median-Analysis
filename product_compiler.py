35500785f4558f4d1ccba007fa8e03eaed123c61
Sam Zimmerman <samzimmerman@me.com>
2023-04-19 21:18:05 -0700

    Add product mapper

--- product_compiler.py
+++ product_compiler.py
@@ -8,18 +8,26 @@ from tqdm import tqdm
 import datetime
 import itertools as itr
 from glob import glob
+import yaml
 
 files = glob("json/AllSetfiles/*.json")
 t = tqdm(files)
+products = []
 for file in t:
+	'''
 	with open(file, 'rb') as ifile:
 		booster_types = ijson.kvitems(ifile, "data.booster")
 		types = list(dict(booster_types).keys())
 		for t in types:
 			print(file, "booster:", t)
+	'''
 	with open(file, 'rb') as ifile:
 		sealed_product = list(ijson.items(ifile, "data.sealedProduct.item"))
 		for p in sealed_product:
-			print(file, p["category"], p.get("subtype"), p["name"])
+			p.update({"contents":[]})
+			products.append(p)
 t.close
 del(t)
+
+with open("products.yaml", 'w') as write:
+	yaml.dump(products, write)
